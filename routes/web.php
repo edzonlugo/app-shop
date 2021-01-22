e316f00894650ea57f7c5c6dca2ef7da1a4a6330
Edzon Alejandro Lugo Torres <edzon.lugo@inai.org.mx>
2021-01-21 18:07:08 -0600

    Base project

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'TestController@welcome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
